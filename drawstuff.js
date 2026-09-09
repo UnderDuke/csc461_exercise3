557274c0adccf029298a8dcbf94e41486e8f460c
UnderDuke <86170302+UnderDuke@users.noreply.github.com>
2026-09-09 14:20:07 -0400

    Refactor light position vector subtraction

--- drawstuff.js
+++ drawstuff.js
@@ -307,7 +307,7 @@ function interpRect(imagedata,top,bottom,left,right,globals,tlAttribs,trAttribs,
         lVect = Vector.subtract(lVect,worldLoc);
         lVect = Vector.normalize(lVect);
         var NdotL = Vector.dot(lVect,new Vector(0,0,1)); // rect in xy plane
-        var H = Vector.normalize(globals.lightPos.subtract(worldLoc));
+        var H = Vector.normalize(Vector.subtract(globals.lightPos, worldLoc));
         var NdotH = Vector.dot(H, new Vector(0,0,1));
 
         // calc ambient
